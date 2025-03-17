6aadd1dd34172d83a0148e20f8b4cec7784e376d
George Ramaniuk <george.ramaniuk@gmail.com>
2025-03-17 15:31:00 +0300

    Add homework #1

--- homework/data_types/homework_test.go
+++ homework/data_types/homework_test.go
@@ -2,47 +2,142 @@ package main
 
 import (
 	"testing"
+	"unsafe"
 
 	"github.com/stretchr/testify/assert"
 )
 
 // go test -v homework_test.go
 
-func ToLittleEndian(number uint32) uint32 {
-	return 0 // need to implement
+type Integer interface {
+	uint16 | uint32 | uint64
 }
 
-func TestСonversion(t *testing.T) {
-	tests := map[string]struct {
-		number uint32
-		result uint32
-	}{
-		"test case #1": {
-			number: 0x00000000,
-			result: 0x00000000,
-		},
-		"test case #2": {
-			number: 0xFFFFFFFF,
-			result: 0xFFFFFFFF,
-		},
-		"test case #3": {
-			number: 0x00FF00FF,
-			result: 0xFF00FF00,
-		},
-		"test case #4": {
-			number: 0x0000FFFF,
-			result: 0xFFFF0000,
-		},
-		"test case #5": {
-			number: 0x01020304,
-			result: 0x04030201,
-		},
+func ToLittleEndian[T Integer](number T) T {
+	if number == 0 {
+		return number
 	}
 
-	for name, test := range tests {
-		t.Run(name, func(t *testing.T) {
-			result := ToLittleEndian(test.number)
-			assert.Equal(t, test.result, result)
-		})
+	var result T
+
+	size := int(unsafe.Sizeof(number))
+
+	for i := 0; i < size; i++ {
+		currentByte := byte(number >> (8 * i) & 0xFF)
+		shift := 8 * (size - i - 1)
+		result |= T(currentByte) << shift
 	}
+
+	return result
+}
+
+func TestConversion(t *testing.T) {
+	t.Run("uint16", func(t *testing.T) {
+		tests := map[string]struct {
+			number uint16
+			result uint16
+		}{
+			"test case #1": {
+				number: 0x0000,
+				result: 0x0000,
+			},
+			"test case #2": {
+				number: 0xFFFF,
+				result: 0xFFFF,
+			},
+			"test case #3": {
+				number: 0x00FF,
+				result: 0xFF00,
+			},
+			"test case #4": {
+				number: 0xFF00,
+				result: 0x00FF,
+			},
+			"test case #5": {
+				number: 0x0102,
+				result: 0x0201,
+			},
+		}
+
+		for name, test := range tests {
+			t.Run(name, func(t *testing.T) {
+				result := ToLittleEndian(test.number)
+
+				assert.Equal(t, test.result, result)
+			})
+		}
+	})
+
+	t.Run("uint32", func(t *testing.T) {
+		tests := map[string]struct {
+			number uint32
+			result uint32
+		}{
+			"test case #1": {
+				number: 0x00000000,
+				result: 0x00000000,
+			},
+			"test case #2": {
+				number: 0xFFFFFFFF,
+				result: 0xFFFFFFFF,
+			},
+			"test case #3": {
+				number: 0x00FF00FF,
+				result: 0xFF00FF00,
+			},
+			"test case #4": {
+				number: 0x0000FFFF,
+				result: 0xFFFF0000,
+			},
+			"test case #5": {
+				number: 0x01020304,
+				result: 0x04030201,
+			},
+		}
+
+		for name, test := range tests {
+			t.Run(name, func(t *testing.T) {
+				result := ToLittleEndian(test.number)
+
+				assert.Equal(t, test.result, result)
+			})
+		}
+	})
+
+	t.Run("uint64", func(t *testing.T) {
+		tests := map[string]struct {
+			number uint64
+			result uint64
+		}{
+			"test case #1": {
+				number: 0x0000000000000000,
+				result: 0x0000000000000000,
+			},
+			"test case #2": {
+				number: 0xFFFFFFFFFFFFFFFF,
+				result: 0xFFFFFFFFFFFFFFFF,
+			},
+			"test case #3": {
+				number: 0x00FF00FF00FF00FF,
+				result: 0xFF00FF00FF00FF00,
+			},
+			"test case #4": {
+				number: 0x00000000FFFFFFFF,
+				result: 0xFFFFFFFF00000000,
+			},
+			"test case #5": {
+				number: 0x0102030405060708,
+				result: 0x0807060504030201,
+			},
+		}
+
+		for name, test := range tests {
+			t.Run(name, func(t *testing.T) {
+				result := ToLittleEndian(test.number)
+
+				assert.Equal(t, test.result, result)
+			})
+		}
+	})
+
 }
