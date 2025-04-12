9569c358a3f815b23afad129f971b6f43f696153
George Ramaniuk <george.ramaniuk@gmail.com>
2025-04-12 10:50:02 +0300

    Add homework #9

--- homework/generics_and_reflection/homework_test.go
+++ homework/generics_and_reflection/homework_test.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 // go test -v homework_test.go
+
+const (
+	keyTag        = "properties"
+	emptyTagValue = "omitempty"
+)
 
 type Person struct {
 	Name    string `properties:"name"`
@@ -16,8 +25,62 @@ type Person struct {
 }
 
 func Serialize(person Person) string {
-	// need to implement
-	return ""
+	personType := reflect.TypeOf(person)
+	personValue := reflect.ValueOf(person)
+
+	sb := strings.Builder{}
+	for i := 0; i < personType.NumField(); i++ {
+		field := personType.Field(i)
+		tagValue := field.Tag.Get(keyTag)
+		if tagValue == "" {
+			continue
+		}
+
+		fieldValue := personValue.Field(i)
+		fieldName, omitEmpty := parseTagValue(tagValue)
+		if fieldName == "" || fieldValue.IsZero() && omitEmpty {
+			continue
+		}
+
+		var strValue string
+		switch fieldValue.Kind() {
+		case reflect.String:
+			strValue = fieldValue.String()
+		case reflect.Int:
+			strValue = strconv.FormatInt(fieldValue.Int(), 10)
+		case reflect.Bool:
+			strValue = strconv.FormatBool(fieldValue.Bool())
+		default:
+			strValue = fmt.Sprintf("%v", fieldValue)
+		}
+
+		if sb.Len() > 0 {
+			sb.WriteByte('\n')
+		}
+		sb.WriteString(fieldName)
+		sb.WriteByte('=')
+		sb.WriteString(strValue)
+	}
+
+	return sb.String()
+}
+
+func parseTagValue(tagValue string) (fieldName string, omitEmpty bool) {
+	for _, part := range strings.Split(tagValue, ",") {
+		str := strings.TrimSpace(part)
+
+		if str == "" {
+			continue
+		}
+
+		if str == emptyTagValue {
+			omitEmpty = true
+			continue
+		}
+
+		fieldName = str
+	}
+	return
 }
 
 func TestSerialization(t *testing.T) {
